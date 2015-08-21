9ded53d8e40648997ffa56cdfe2f4118b29136f2
MrL <lalit.umbarkar9@gmail.com>
2015-08-21 19:19:51 +0530

    Added Version 0.2.3

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # File-Renamer
 Renames the files before download at academics.vit.ac.in  
+
+#Installation
+##Chrome
+
+Download [crx file](http://thecoderguy.host22.com/File-Renamer.crx) in your PC.
+Drag and drop this file in Chrome.
+Agree to add the extension.
+And Voila it will start renaming on its own.
+
+
---
+++ rename_bg.js
@@ -0,0 +1,31 @@
+
+
+chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+  
+  var regex = /(FALL|WIN|SUM){1}SEM[0-9]{4}-[0-9]{2}_CP[0-9]{4}.*_[A-Z]{2}[0-9]{2}_/;
+  var text = item.filename; 
+  var label;
+  
+  if (regex.test(text)){
+    label = text.split(regex.exec(text)[0])[1];
+    suggest({filename:label, 
+            conflict_action: 'overwrite',
+            conflictAction: 'overwrite'});
+  }
+  
+  suggest({filename:text, 
+          conflict_action: 'overwrite',
+          conflictAction: 'overwrite'});
+    
+});
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-66597095-1', 'auto');
+ga('send', 'pageview');
+
+
